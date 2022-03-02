027eeb6ef85c0f1c92929c91ea8fa007a791202d
Mitesh Shah <58204159+mitsha-microsoft@users.noreply.github.com>
2022-03-02 19:10:24 +0530

    Changing the mock for StorageBlob

--- Scenario3/PetImagesTest/StorageMocks/MockBlobContainer.cs
+++ Scenario3/PetImagesTest/StorageMocks/MockBlobContainer.cs
@@ -7,6 +7,7 @@ using PetImages.Storage;
 
 namespace PetImagesTest.StorageMocks
 {
+    // Buggy mock, need to fix these mocks as part of third scenario.
     internal class MockBlobContainerProvider : IBlobContainer
     {
         private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, byte[]>> Containers;
@@ -18,91 +19,66 @@ namespace PetImagesTest.StorageMocks
 
         public Task CreateContainerAsync(string containerName)
         {
-            return Task.Run(() =>
-            {
-                this.Containers.TryAdd(containerName, new ConcurrentDictionary<string, byte[]>());
-            });
+            return Task.FromResult(this.Containers.TryAdd(containerName, new ConcurrentDictionary<string, byte[]>()));
         }
 
         public Task CreateContainerIfNotExistsAsync(string containerName)
         {
-            return Task.Run(() =>
-            {
-                this.Containers.TryAdd(containerName, new ConcurrentDictionary<string, byte[]>());
-            });
+            return Task.FromResult(this.Containers.TryAdd(containerName, new ConcurrentDictionary<string, byte[]>()));
         }
 
         public Task DeleteContainerAsync(string containerName)
         {
-            return Task.Run(() =>
-            {
-                this.Containers.TryRemove(containerName, out ConcurrentDictionary<string, byte[]> _);
-            });
+            return Task.FromResult(this.Containers.TryRemove(containerName, out ConcurrentDictionary<string, byte[]> _));
         }
 
         public Task<bool> DeleteContainerIfExistsAsync(string containerName)
         {
-            return Task.Run(() =>
-                {
-                    return this.Containers.TryRemove(containerName, out ConcurrentDictionary<string, byte[]> _);
-                });
+            return Task.FromResult(this.Containers.TryRemove(containerName, out ConcurrentDictionary<string, byte[]> _));
         }
 
         public Task CreateOrUpdateBlobAsync(string containerName, string blobName, byte[] blobContents)
         {
-            return Task.Run(() =>
-            {
-                this.Containers[containerName].AddOrUpdate(blobName, blobContents, (_, oldContents) => blobContents);
-            });
+            return Task.FromResult(this.Containers[containerName].AddOrUpdate(blobName, blobContents, (_, oldContents) => blobContents));
         }
 
         public Task<byte[]> GetBlobAsync(string containerName, string blobName)
         {
-            return Task.Run(() =>
-            {
-                return this.Containers[containerName][blobName];
-            });
+            var result = this.Containers[containerName][blobName];
+            return Task.FromResult(result);
         }
 
         public Task<bool> ExistsBlobAsync(string containerName, string blobName)
         {
-            return Task.Run(() =>
-            {
-                return this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container) &&
+            var finalResult = this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container) &&
                     container.ContainsKey(blobName);
-            });
+            return Task.FromResult(finalResult);
         }
 
         public Task DeleteBlobAsync(string containerName, string blobName)
         {
-            return Task.Run(() =>
-            {
-                this.Containers[containerName].TryRemove(blobName, out byte[] _);
-            });
+            return Task.FromResult(this.Containers[containerName].TryRemove(blobName, out byte[] _));
         }
 
         public Task<bool> DeleteBlobIfExistsAsync(string containerName, string blobName)
         {
-            return Task.Run(() =>
+            bool finalResult;
+            if (!this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container))
             {
-                if (!this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container))
-                {
-                    return false;
-                }
+                finalResult = false;
+            }
 
-                return container.TryRemove(blobName, out byte[] _);
-            });
+            finalResult = container.TryRemove(blobName, out byte[] _);
+            return Task.FromResult(finalResult);
         }
 
         public Task DeleteAllBlobsAsync(string containerName)
         {
-            return Task.Run(() =>
+            if (this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container))
             {
-                if (this.Containers.TryGetValue(containerName, out ConcurrentDictionary<string, byte[]> container))
-                {
-                    container.Clear();
-                }
-            });
+                container.Clear();
+            }
+            return Task.CompletedTask;
         }
     }
 }
