041b2241e77c16edfb0125b90430571144dbcc99
Mitesh Shah <58204159+mitsha-microsoft@users.noreply.github.com>
2022-03-02 19:14:26 +0530

    Removing fault injection from Cosmos Db Mocks

--- Scenario3/PetImagesTest/StorageMocks/MockCosmosContainer.cs
+++ Scenario3/PetImagesTest/StorageMocks/MockCosmosContainer.cs
@@ -2,10 +2,8 @@
 // Licensed under the MIT License.
 
 using System.Threading.Tasks;
-using Microsoft.Coyote.Random;
 using PetImages.Entities;
 using PetImages.Storage;
-using PetImagesTest.Exceptions;
 
 namespace PetImagesTest.StorageMocks
 {
@@ -13,15 +11,11 @@ namespace PetImagesTest.StorageMocks
     {
         private readonly string ContainerName;
         private readonly MockCosmosState State;
-        private readonly Generator Generator;
-        private bool EmitRandomizedFaults;
 
         public MockCosmosContainer(string containerName, MockCosmosState state)
         {
             this.ContainerName = containerName;
             this.State = state;
-            this.Generator = Generator.Create();
-            this.EmitRandomizedFaults = false;
         }
 
         public Task<T> CreateItem<T>(T item)
@@ -31,11 +25,6 @@ namespace PetImagesTest.StorageMocks
 
             return Task.Run(() =>
             {
-                if (this.EmitRandomizedFaults && this.Generator.NextBoolean())
-                {
-                    throw new SimulatedDatabaseFaultException();
-                }
-
                 this.State.CreateItem(this.ContainerName, itemCopy);
                 return itemCopy;
             });
@@ -46,11 +35,6 @@ namespace PetImagesTest.StorageMocks
         {
             return Task.Run(() =>
             {
-                if (this.EmitRandomizedFaults && this.Generator.NextBoolean())
-                {
-                    throw new SimulatedDatabaseFaultException();
-                }
-
                 var item = this.State.GetItem(this.ContainerName, partitionKey, id);
 
                 var itemCopy = TestHelper.Clone((T)item);
@@ -64,11 +48,6 @@ namespace PetImagesTest.StorageMocks
         {
             return Task.Run(() =>
             {
-                if (this.EmitRandomizedFaults && this.Generator.NextBoolean())
-                {
-                    throw new SimulatedDatabaseFaultException();
-                }
-
                 var itemCopy = TestHelper.Clone(item);
                 this.State.UpsertItem(this.ContainerName, itemCopy);
                 return itemCopy;
@@ -79,24 +58,8 @@ namespace PetImagesTest.StorageMocks
         {
             return Task.Run(() =>
             {
-                if (this.EmitRandomizedFaults && this.Generator.NextBoolean())
-                {
-                    throw new SimulatedDatabaseFaultException();
-                }
-
                 this.State.DeleteItem(this.ContainerName, partitionKey, id);
             });
         }
-
-        public void EnableRandomizedFaults()
-        {
-            this.EmitRandomizedFaults = true;
-        }
-
-        public void DisableRandomizedFaults()
-        {
-            this.EmitRandomizedFaults = false;
-        }
-
     }
 }
