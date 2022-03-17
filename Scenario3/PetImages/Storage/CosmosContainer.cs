ec1204c272e4e2a45f2e1aa1c0b0fa5b403a818b
Mitesh Shah <58204159+mitsha-microsoft@users.noreply.github.com>
2022-03-17 19:10:50 +0530

    EmulatorBased Cosmos Client Added

---
+++ Scenario3/PetImages/Storage/CosmosContainer.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using PetImages.Entities;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+using PetImages.Exceptions;
+
+namespace PetImages.Storage
+{
+    public class CosmosContainer : ICosmosContainer
+    {
+        private readonly Container cosmosContainer;
+
+        public CosmosContainer(Container cosmosContainer)
+        {
+            this.cosmosContainer = cosmosContainer;
+        }
+
+        public async Task<T> CreateItem<T>(T row) where T : DbItem
+        {
+            var response = await this.cosmosContainer.CreateItemAsync(row);
+            return response.Resource;
+        }
+
+        public async Task DeleteItem(string partitionKey, string id, string ifMatchEtag = null)
+        {
+            var response = await this.cosmosContainer.DeleteItemAsync<DbItem>(
+                id,
+                new PartitionKey(partitionKey),
+                new ItemRequestOptions() { IfMatchEtag = ifMatchEtag });
+
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new DatabaseItemDoesNotExistException();
+            }
+        }
+
+        public async Task<T> GetItem<T>(string partitionKey, string id) where T : DbItem
+        {
+            var response = await this.cosmosContainer.ReadItemAsync<T>(id, new PartitionKey(partitionKey));
+            
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new DatabaseItemDoesNotExistException();
+            }
+
+            return response.Resource;
+        }
+
+        public async Task<T> ReplaceItem<T>(T row, string ifMatchEtag = null) where T : DbItem
+        {
+            var response = await this.cosmosContainer.ReplaceItemAsync(
+                row,
+                row.Id,
+                new PartitionKey(row.PartitionKey),
+                new ItemRequestOptions() { IfMatchEtag = ifMatchEtag });
+            
+            // TODO: Throw appropriate exceptions
+            return response.Resource;
+        }
+
+        public async Task<T> UpsertItem<T>(T row, string ifMatchEtag = null) where T : DbItem
+        {
+            var response = await this.cosmosContainer.UpsertItemAsync(
+                row,
+                new PartitionKey(row.PartitionKey),
+                new ItemRequestOptions() { IfMatchEtag = ifMatchEtag });
+            
+            // TODO: Throw appropriate exceptions
+            return response.Resource;
+        }
+    }
+}
---
+++ Scenario3/PetImages/Storage/CosmosDatabase.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using PetImages.Entities;
+
+namespace PetImages.Storage
+{
+    public class CosmosDatabase : ICosmosDatabase
+    {
+        private readonly CosmosClient cosmosClient;
+
+        private Database cosmosDatabase;
+
+        private readonly string databaseName;
+
+        public static async Task<ICosmosDatabase> CreateAsync(string databaseName)
+        {
+            var instance = new CosmosDatabase(databaseName);
+            await instance.Initialize();
+            return instance;
+        }
+
+        public CosmosDatabase(string databaseName)
+        {
+            // Connect to the Azure Cosmos DB Emulator running locally
+            // Ideally the endpoint and key should come from config
+            this.cosmosClient = new CosmosClient(
+               "https://localhost:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+            this.databaseName = databaseName;
+        }
+
+        public async Task<ICosmosContainer> CreateContainerAsync(string containerName)
+        {
+            await this.cosmosDatabase.CreateContainerIfNotExistsAsync(
+                new ContainerProperties
+                {
+                    Id = containerName,
+                    PartitionKeyPath = nameof(DbItem.PartitionKey),
+                });
+
+            var cosmosContainer = this.cosmosDatabase.GetContainer(containerName);
+            return new CosmosContainer(cosmosContainer);
+        }
+
+        public Task<ICosmosContainer> GetContainer(string containerName)
+        {
+            var cosmosContainer = this.cosmosDatabase.GetContainer(containerName);
+            
+            // Since getting container is not async
+            return Task.FromResult((ICosmosContainer) new CosmosContainer(cosmosContainer));
+        }
+
+        private async Task Initialize()
+        {
+            await this.cosmosClient.CreateDatabaseIfNotExistsAsync(this.databaseName);
+            this.cosmosDatabase = this.cosmosClient.GetDatabase(this.databaseName);
+        }
+    }
+}
